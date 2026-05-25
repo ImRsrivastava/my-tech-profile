6adc0f200ece014332389b19daf8b9b492c218d6
ImRsrivastava <rishabh.srivastava0205@gmail.com>
2026-05-25 07:31:20 +0530

    corrected profile tag line

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -30,7 +30,7 @@ const Index = () => {
                         <div className="offset-3 col-lg-8">
                             <h2><i> Rishabh Srivastava </i></h2>
                             <p>I'm a &nbsp;
-                                <span ref={el} className="typed" data-typed-items="AWS Cloud & DevOps Engineer, AWS Infra Engineer, Senior Backend Engineer, Cloud & Infrastructure Engineer"></span>
+                                <span ref={el} className="typed" data-typed-items="AWS Cloud and DevOps Engineer, AWS Infra Engineer, Senior Backend Engineer, Cloud and Infrastructure Engineer"></span>
                                 <span className="typed-cursor typed-cursor--blink" aria-hidden="true"></span>
                             </p>
                             <div className="social-links">
